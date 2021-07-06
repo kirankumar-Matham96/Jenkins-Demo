17cbcfea791707bb05b194ea768d25aeb4347c50
kirankumae-Matham96 <mathamkirankumar96@gamil.com>
2021-07-06 23:26:07 +0530

    2nd line added

--- Hello.java
+++ Hello.java
@@ -2,6 +2,7 @@ import java.lang.*;
 import java.util.*;
 public class Hello{
 public static void main(String[] args){
-	System.out.println("Hello jenkins!");
+	System.out.println("Hello jenkins1!");
+	System.out.println("Hello jenkins2!");
 }
 }
